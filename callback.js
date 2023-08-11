d7bda0139091916635cc47a1bf43440482a6a128
mutoni <uwingeneyenazifa29@gmail.com>
2023-08-11 15:44:20 +0200

    add

---
+++ callback.js
@@ -0,0 +1,10 @@
+let tata=["gana","dabl","waw"];
+function bam(aaa){
+    setTimeout(function(){
+        tata.push(aaa)
+        // console.log()
+        console.log(tata)
+}
+     ),5000 };
+
+bam("hh")
